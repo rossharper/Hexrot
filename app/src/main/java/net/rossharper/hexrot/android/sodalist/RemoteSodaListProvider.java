7b9b99e1e02d360baa918da7da8683c0f6c8f22b
Ross Harper <rossharper@users.noreply.github.com>
2015-01-22 17:40:41 +0000

    Adding json parser
    TODO:
    - need to split out providers and fetchers. Make them hexagonal
    - UNIT TEST SOME OF THIS!

--- app/src/main/java/net/rossharper/hexrot/android/sodalist/RemoteSodaListProvider.java
+++ app/src/main/java/net/rossharper/hexrot/android/sodalist/RemoteSodaListProvider.java
@@ -1,7 +1,10 @@
 package net.rossharper.hexrot.android.sodalist;
 
+import net.rossharper.hexrot.sodalist.SodaList;
 import net.rossharper.hexrot.sodalist.SodaListProvider;
 import net.rossharper.hexrot.sodalist.SodaListProviderListener;
+
+import org.json.JSONException;
 
 public class RemoteSodaListProvider implements SodaListProvider {
     @Override
@@ -9,7 +12,13 @@ public class RemoteSodaListProvider implements SodaListProvider {
         new GithubJsonSodaListFetcher().getSodas(new GithubJsonSodaListFetcher.Listener() {
             @Override
             public void sodasReceived(String jsonSodaList) {
-                sodaListProviderListener.sodaListReceived(new SodaJsonParser().parse(jsonSodaList));
+                SodaList sodaList = null;
+                try {
+                    sodaList = new SodaJsonParser().parse(jsonSodaList);
+                    sodaListProviderListener.sodaListReceived(sodaList);
+                } catch (JSONException e) {
+                    // TODO: error handling
+                }
             }
         });
     }
--- app/src/main/java/net/rossharper/hexrot/android/sodalist/SodaJsonParser.java
+++ app/src/main/java/net/rossharper/hexrot/android/sodalist/SodaJsonParser.java
@@ -3,12 +3,30 @@ package net.rossharper.hexrot.android.sodalist;
 import net.rossharper.hexrot.model.Soda;
 import net.rossharper.hexrot.sodalist.SodaList;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.ArrayList;
+import java.util.List;
 
 public class SodaJsonParser {
-    public SodaList parse(String response) {
-        SodaList sodaList = new SodaList(new ArrayList<Soda>());
+    public SodaList parse(String response) throws JSONException {
+        List<Soda> sodaList = new ArrayList<Soda>();
 
-        return sodaList;
+        JSONObject rootJsonObject = new JSONObject(response);
+        JSONArray jsonArray = rootJsonObject.getJSONArray("sodaList");
+        for(int i = 0; i < jsonArray.length(); ++i) {
+            JSONObject jsonObject = jsonArray.getJSONObject(i);
+            final String sodaName = jsonObject.getString("name");
+            sodaList.add(new Soda() {
+                @Override
+                public String getName() {
+                    return sodaName;
+                }
+            });
+        }
+
+        return new SodaList(sodaList);
     }
 }
