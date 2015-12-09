3ae68ade8ed9466caa06da9b88686251ec7e9774
Ross Harper <ross.harper@bbc.co.uk>
2015-12-09 10:16:49 +0000

    Extract fetcher interface

--- hexrotcore/src/main/java/net/rossharper/hexrot/networking/StringFetcher.java
+++ hexrotcore/src/main/java/net/rossharper/hexrot/networking/StringFetcher.java
@@ -3,11 +3,6 @@ package net.rossharper.hexrot.networking;
 /**
  * Created by harper05 on 09/12/15.
  */
-public interface StringFetcher {
-    void get(String url, ResponseListener responseListener);
+public interface StringFetcher extends Fetcher {
 
-    public static interface ResponseListener {
-        void onResponse(String response);
-        void onError();
-    }
 }
